c9984933ecafcf531af1533b013f7ef68cf19dea
mackinitup <44298083+mackinitup@users.noreply.github.com>
2018-12-02 23:21:57 -0500

    Add files via upload

---
+++ John Mackin Mini-Project 3.py
@@ -0,0 +1,68 @@
+
+
+
+name1 = raw_input("Input a name. ")
+adverb1 = raw_input("Input an adverb. ")
+theme1 = raw_input("Input a theme. ")
+place1 = raw_input("Input a place. ")
+dayofweek1 = raw_input("Input a day of the week. ")
+time1 = raw_input("Input a time of day. ")
+verb1 = raw_input("Input a verb. ")
+animal1 = raw_input("Input an animal. ")
+body1 = raw_input("Input a body part. ")
+rsvp1 = raw_input("Input a phone number. ")
+name0 = raw_input("Input your name. ")
+name2 = raw_input("Input your crush's name. ")
+pluralnoun1 = raw_input("Input a plural noun. ")
+pluralnoun2 = raw_input("Input another plural noun. ")
+pluralnoun3 = raw_input("Input another plural noun. ")
+date1 = raw_input("Input a date. ")
+badadj1 = raw_input("Input a demeaning adjective. ")
+subject1 = raw_input("Input your favorite subject in school. ")
+mom1 = raw_input("Input your mom's full name. ")
+fluid1 = raw_input("Input a type of fluid. ")
+substance1 = raw_input("Input a pharmaceutical/recreational substance. ")
+subject2 = raw_input("Input your least favorite subject here. ")
+adj1 = raw_input("Input an adjective here. " )
+person21 = raw_input("Input the name of someone in the room. ")
+clothing1 = raw_input("Input an article of clothing. ")
+event2 = raw_input("Input a fun event. ")
+
+
+variables = [name1, adverb1]
+for x in variables:
+	print " Dear " + name1 + ", You are " + adverb1 + " invited!"
+	print name1 + " is having a " +theme1 + " party! "
+	
+print " It's going to be at " + place1 + " on " + dayofweek1 + ". " 
+print " Please make sure to show up at " + time1 + ", or else you will be required to " + verb1 + " a/an " + animal1 + " with your " + body1 + "." 
+print " RSVP at " + rsvp1 + "." 
+
+print " Dear " + name0 + ", " + pluralnoun1 + " are red, " + pluralnoun2 + " are blue, " 
+print " You love me and I love " + pluralnoun3 + "! " 
+print " From, " + name2
+
+print " Date: " + date1
+print " Please excuse " + name0 + ", who is far too " + badadj1 + " to attend " + subject1 + " class. " 
+print " Signed: " + mom1 
+
+print name0 + " is sick with the " + body1 + " flu. " 
+print " Drink more " + fluid1 + " and take " + substance1 + " as needed. "
+print " Signed: " + mom1
+
+print " Date: " + date1
+print name0 + " is authorized to be at " + place1 + " instead of " + subject2 + " class. "
+
+print " Date: " + date1
+print name0 + " is too cool for " + subject2 + " class. " 
+print " Instead, she/he will be attending the " + event2 + ". "
+print " Signed: " + mom1
+
+print " Signs point to a very " + adj1 + " yes. "
+
+print " Don't believe anything " + person21 + " says. " 
+
+print "You will find the answer in your " + clothing1 + "." 
+
+
+
